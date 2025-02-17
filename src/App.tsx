8c6917e1786d8258764c82179a206ede47d64e5f
suraphon <suraphon.k@merudy.com>
2025-02-17 11:14:23 +0700

    first init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+   return (
+      <>
+         <h1>{React.version}</h1>
+         <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      </>
+   )
+}
